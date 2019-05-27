fee569247bf1542f628b4d09797a5664c50ded55
jiangmianfeng <1315454038@qq.com>
2019-05-27 13:49:18 +0800

    2

---
+++ OA/WJQ.OA.Model/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WJQ.OA.Model
+{
+    public class Class1
+    {
+    }
+}
---
+++ OA/WJQ.OA.WebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WJQ.OA.WebApp.MvcApplication" Language="C#" %>
